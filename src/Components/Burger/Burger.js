0463335d17fce914baf3c6d6ef59575e5de3deda
Paras Buda <parasbuda1234@gmail.com>
2020-08-28 19:58:31 +0545

    adding control is started

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -2,11 +2,16 @@ import React from "react"
 import "./Burger.css"
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
 const Burger = (props) => {
-  const transformedIngredient = Object.keys(props.ingredient).map((ing) => {
+  let transformedIngredient = Object.keys(props.ingredient).map((ing) => {
     return [...Array(props.ingredient[ing])].map((_, i) => {
       return <BurgerIngredient key={ing + i} type={ing} />
     })
-  })
+  }).reduce((acc, el) => {
+    return acc.concat(el)
+  }, [])
+  if (transformedIngredient.length === 0) {
+    transformedIngredient=<p>Please add some ingredient</p>
+  }
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
